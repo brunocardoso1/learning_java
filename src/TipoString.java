9eb5a9c4d4fa20bb792ec1f20c557ebf007b9f7a
Bruno Cardoso <bruno.alvescardoso@hotmail.com>
2022-06-22 22:47:23 -0300

    project update

---
+++ src/TipoString.java
@@ -0,0 +1,15 @@
+public class TipoString {
+
+    public static void main(String [] args){
+        System.out.println("Olá pessoal". charAt(1));
+
+        String s = "Boa tarde";
+        System.out.println(s.concat("!!!"));
+
+        System.out.println(s.startsWith("Boa"));
+
+        System.out.println(s.equalsIgnoreCase("bOA taRde"));
+
+    }
+
+}
